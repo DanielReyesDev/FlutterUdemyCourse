d605d3993d04fe721f94bbe129b65b7258b8cd40
Daniel Reyes <dreyes.developer@gmail.com>
2018-07-28 10:38:08 -0500

    learning about debugging

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter/rendering.dart'; // Required only for debbuging
 import './product_manager.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  // debugPaintSizeEnabled = true;
+  // debugPaintBaselinesEnabled = true;
+  // debugPaintPointersEnabled = true;
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
     Widget build(BuildContext context) {
       // TODO: implement build
       return MaterialApp(
+        // debugShowMaterialGrid: true,
         theme: ThemeData(
           brightness: Brightness.light,
           primarySwatch: Colors.deepOrange,
